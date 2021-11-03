dacaa2d887ae4d3e74f129f25f68adf885c11e92
rom <mozzieisme@gmail.com>
2021-11-02 20:28:08 -0400

    Create a new file

---
+++ survey.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+rl.question("What's your name? Nicknames are also acceptable :) ", (answer1) => {
+  console.log(`${answer1}`);
+  rl.question("What's an activity you like doing? ", (answer2) => {
+    console.log(`${answer2}`);
+    rl.question("What do you listen to while doing that? ", (answer3) => {
+      console.log(`${answer3}`);
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer4) => {
+        console.log(`${answer4}`);
+        rl.question("What's your favourite thing to eat for that meal? ", (answer5) => {
+          console.log(`${answer5}`);
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+            console.log(`${answer6}`);
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer7) => {
+              console.log(`${answer7}`);
+              console.log(`${answer1} Thank you for your valuable feedback:`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
+
